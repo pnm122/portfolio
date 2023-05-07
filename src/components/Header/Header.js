4979450cdc06f918c832c639e714a15719cdfefd
pnm122 <pnm12@pitt.edu>
2023-05-07 04:05:55 -0400

    Made header mobile-friendly

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 export default function Header({selected}) {
+
+  const [expanded, setExpanded] = useState(false);
 
   const setHeaderDisplay = e => {
     let header = document.getElementById(styles.headerOuter);
@@ -28,11 +30,15 @@ export default function Header({selected}) {
     <header id={styles.headerOuter}>
       <div className={`container ${styles.headerInner}`}>
         <Link to="/" id={styles.name}>Pierce Martin</Link>
-        <div id={styles.links}>
-          <a href="#" id={selected == 0 ? styles.selected : null}><span>Resume</span></a>
-          <Link to="/projects" id={selected == 1 ? styles.selected : null}><span>Projects</span></Link>
-          <a href="#contact" id={selected == 2 ? styles.selected : null}><span>Contact Me</span></a>
+        <div id={styles.links} aria-expanded={expanded}>
+          <a href="#" id={selected == 0 ? styles.selected : null} onClick={() => setExpanded(false)}><span>Resume</span></a>
+          <Link to="/projects" id={selected == 1 ? styles.selected : null} onClick={() => setExpanded(false)}><span>Projects</span></Link>
+          <a href="#contact" id={selected == 2 ? styles.selected : null} onClick={() => setExpanded(false)}><span>Contact Me</span></a>
         </div>
+        <button aria-expanded={expanded} id={styles.dropdownButton} onClick={() => setExpanded(!expanded)}>
+          <div />
+          <div />
+        </button>
       </div>
     </header>
   )
